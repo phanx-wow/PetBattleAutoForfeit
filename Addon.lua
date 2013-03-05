7159607cd10af02c9a5064e43fcc0c87ffb41edf
Phanx <addons@phanx.net>
2013-03-05 10:03:53 +0000

    - Use LibPetJournal-2.0

--- Addon.lua
+++ Addon.lua
@@ -27,8 +27,6 @@ end
 
 ------------------------------------------------------------------------
 
-local petLevel, petQuality = {}, {}
-
 local SPECIES_NOT_CAPTURABLE = {
 	[52] = true, -- Ancona Chicken
 }
@@ -66,7 +64,6 @@ end)
 function f:HookButton()
 	if PetBattleFrame and PetBattleFrame.BottomFrame and PetBattleFrame.BottomFrame.ForfeitButton then
 		PetBattleFrame.BottomFrame.ForfeitButton:SetScript("OnClick", function(...)
-			-- print("ForfeitButton OnClick", IsShiftKeyDown())
 			if IsShiftKeyDown() then
 				C_PetBattles.ForfeitGame()
 			else
@@ -77,51 +74,43 @@ function f:HookButton()
 	end
 end
 
+function f:GetBestPetInfo(species)
+	local best, highest = 0, 0
+	for _, petID in LibStub("LibPetJournal-2.0"):IteratePetIDs() do
+		local speciesID, _, level = C_PetJournal.GetPetInfoByPetID(petID)
+		if speciesID == species then
+			local _, _, _, _, quality = C_PetJournal.GetPetStats(petID)
+			if quality >= best then
+				best = quality
+				highest = max(highest, level)
+			end
+		end
+	end
+	return best, highest
+end
+
 f:RegisterEvent("PET_BATTLE_OPENING_START")
-f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function(self, event, name)
 	if self.HookButton then
 		self:HookButton()
 	end
-
-	if event == "PLAYER_LOGIN" then
-		LibStub("LibPetScanner-1.0"):RegisterScan(function()
-			wipe(petLevel)
-			wipe(petQuality)
-
-			local _, numPets = C_PetJournal.GetNumPets(true)
-			--print("Found pets:", numPets)
-			for i = 1, numPets do
-				local id, species, owned, _, level = C_PetJournal.GetPetInfoByIndex(i)
-				if owned then
-					local _, _, _, _, quality = C_PetJournal.GetPetStats(id)
-					if not petLevel[species] or level > petLevel[species] then
-						petLevel[species] = level
-					end
-					if not petQuality[species] or quality > petQuality[species] then
-						petQuality[species] = quality
-					end
-				end
-			end
-		end)
-		return self:UnregisterEvent(event)
-
-	elseif PBAF_ENABLE and C_PetBattles.IsWildBattle() then
+	if PBAF_ENABLE and C_PetBattles.IsWildBattle() then
 		local upgrade
 		for i = 1, C_PetBattles.GetNumPets(LE_BATTLE_PET_ENEMY) do
 			local species = C_PetBattles.GetPetSpeciesID(LE_BATTLE_PET_ENEMY, i)
 			if not SPECIES_NOT_CAPTURABLE[species] then
 				local quality = C_PetBattles.GetBreedQuality(LE_BATTLE_PET_ENEMY, i)
 				if quality >= PBAF_MIN_QUALITY then
-					if not petQuality[species] then
+					local bestQuality, bestLevel = f:GetBestPetInfo(species)
+					if not bestQuality then
 						upgrade = true
 						break
-					elseif quality > petQuality[species] then
+					elseif quality > bestQuality then
 						upgrade = true
 						break
-					elseif quality == petQuality[species] then
+					elseif quality == bestQuality then
 						local level = C_PetBattles.GetLevel(LE_BATTLE_PET_ENEMY, i)
-						if level - PBAF_MIN_LEVEL_DIFF >= petLevel[species] then
+						if level - PBAF_MIN_LEVEL_DIFF >= bestLevel then
 							upgrade = true
 							break
 						end
